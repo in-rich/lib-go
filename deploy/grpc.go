6f5d86306295af0efc43166e0a48cbc3c1462f26
Geoffroy Vincent <kuzanisu@gmail.com>
2024-09-16 03:30:29 +0200

    fix: update GRPC methods to work in Cloud Run environment

--- deploy/grpc.go
+++ deploy/grpc.go
@@ -9,102 +9,25 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"net"
-	"os"
-	"strings"
 )
 
-func loadCertificates(prefix string) (tls.Certificate, error) {
-	certEnv := fmt.Sprintf("%s_CERT", strings.ToUpper(prefix))
-	keyEnv := fmt.Sprintf("%s_KEY", strings.ToUpper(prefix))
+func OpenGRPCConn(host string) *grpc.ClientConn {
+	var opts []grpc.DialOption
 
-	rawCert := os.Getenv(certEnv)
-	rawKey := os.Getenv(keyEnv)
+	if IsReleaseEnv() {
+		systemRoots, err := x509.SystemCertPool()
+		if err != nil {
+			log.Fatal(err, "failed to load system root CA certificates")
+		}
 
-	if rawCert == "" {
-		return tls.Certificate{}, fmt.Errorf("missing certificate for service: %s\n", prefix)
-	}
-	if rawKey == "" {
-		return tls.Certificate{}, fmt.Errorf("missing key for service: %s\n", prefix)
-	}
+		cred := credentials.NewTLS(&tls.Config{RootCAs: systemRoots})
 
-	cert, err := tls.X509KeyPair([]byte(rawCert), []byte(rawKey))
-	if err != nil {
-		return tls.Certificate{}, fmt.Errorf("failed to load certificate for service %s: %w", prefix, err)
+		opts = append(opts, grpc.WithAuthority(host), grpc.WithTransportCredentials(cred))
+	} else {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	}
 
-	return cert, nil
-}
-
-func loadCA(prefix string) (*x509.CertPool, error) {
-	caEnv := fmt.Sprintf("%s_CA", strings.ToUpper(prefix))
-
-	rawCA := os.Getenv(caEnv)
-
-	if rawCA == "" {
-		return nil, fmt.Errorf("missing CA for service: %s\n", prefix)
-	}
-
-	ca := x509.NewCertPool()
-	if ok := ca.AppendCertsFromPEM([]byte(rawCA)); !ok {
-		return nil, fmt.Errorf("failed to load CA for service %s\n", prefix)
-	}
-
-	return ca, nil
-}
-
-func loadClientCredentials(host, name string) credentials.TransportCredentials {
-	if !IsReleaseEnv() {
-		return insecure.NewCredentials()
-	}
-
-	cert, err := loadCertificates(name)
-	if err != nil {
-		log.Fatal(err, "failed to load certificate for service "+name)
-	}
-
-	ca, err := loadCA(name)
-	if err != nil {
-		log.Fatal(err, "failed to load CA for service "+name)
-	}
-
-	tlsConfig := &tls.Config{
-		ServerName:   host,
-		Certificates: []tls.Certificate{cert},
-		RootCAs:      ca,
-	}
-
-	return credentials.NewTLS(tlsConfig)
-}
-
-func loadServerCredentials(name string) credentials.TransportCredentials {
-	if !IsReleaseEnv() {
-		return insecure.NewCredentials()
-	}
-
-	cert, err := loadCertificates(name)
-	if err != nil {
-		log.Fatal(err, "failed to load certificate for service "+name)
-	}
-
-	ca, err := loadCA(name)
-	if err != nil {
-		log.Fatal(err, "failed to load CA for service "+name)
-	}
-
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		ClientCAs:    ca,
-		ClientAuth:   tls.RequireAndVerifyClientCert,
-	}
-
-	return credentials.NewTLS(tlsConfig)
-}
-
-func OpenGRPCConn(host, name string) *grpc.ClientConn {
-	conn, err := grpc.NewClient(
-		host,
-		grpc.WithTransportCredentials(loadClientCredentials(host, name)),
-	)
+	conn, err := grpc.NewClient(host, opts...)
 	if err != nil {
 		log.Fatal(err, "failed to connect to service")
 	}
@@ -118,15 +41,17 @@ func CloseGRPCConn(conn *grpc.ClientConn) {
 	}
 }
 
-func StartGRPCServer(port, name string) (net.Listener, *grpc.Server) {
+func StartGRPCServer(port int) (net.Listener, *grpc.Server) {
+	if port == 0 {
+		log.Fatal("port is required")
+	}
+
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	server := grpc.NewServer(grpc.Creds(loadServerCredentials(name)))
-
-	return listener, server
+	return listener, grpc.NewServer()
 }
 
 func CloseGRPCServer(listener net.Listener, server *grpc.Server) {
